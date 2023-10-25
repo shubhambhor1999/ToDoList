cc59a1a09df7ab20104b22f92059d1888a8e5f2d
shubhambhor1999 <shubhambhor1999@gmail.com>
2023-10-25 21:12:29 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -5,8 +5,6 @@ const app=express();
 const port=process.env.PORT||3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
-app.set("views", "views");
-app.set("view engine", "ejs");
 var heading="";
 var todolist=new Array();
 var todolistToday=new Array();
